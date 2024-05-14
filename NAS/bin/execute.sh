8b7b4916be042a40e3177258fcfb9f77419c79ac
William Maas <williambmaas@gmail.com>
2024-05-14 09:16:32 -0300

    Update execute.sh

--- NAS/bin/execute.sh
+++ NAS/bin/execute.sh
@@ -7,7 +7,7 @@ export OMP_PLACES=cores
 for bench in bt.B.x cg.B.x ft.B.x lu.B.x mg.B.x sp.B.x ua.B.x
 do
 
-for nt in 2 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120 124 128
+for nt in 1 2 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120 124 128
 	do
 	echo "Running $bench with $nt threads"
 		export OMP_NUM_THREADS=$nt
